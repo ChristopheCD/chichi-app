005c5ebf804fb9673058b1aa4dae99cdacd17ea7
Christophe Castaing <christophe.castaing@cdbdx.biz>
2020-12-01 15:25:20 +0100

    feat: 🎸 update Header styles

--- src/Header/index.js
+++ src/Header/index.js
@@ -9,13 +9,17 @@ import {
 import { ShoppingCart } from "@material-ui/icons";
 import { number } from "prop-types";
 
-import "./styles.css";
+import useStyles from "./useStyles";
 
 export default function Header({ shoppingCartItemsCount }) {
+  const classes = useStyles();
+
   return (
     <AppBar position="static">
       <Toolbar>
-        <Typography variant="h6">Chi-Chi Sushis</Typography>
+        <Typography variant="h6" className={classes.title}>
+          Chi-Chi Sushis
+        </Typography>
         <IconButton aria-label="show 17 new notifications" color="inherit">
           <Badge badgeContent={shoppingCartItemsCount} color="secondary">
             <ShoppingCart />
