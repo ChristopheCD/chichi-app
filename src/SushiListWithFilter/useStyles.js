ca782ced0874de68766e66c98891e2681ea86c4a
Christophe Castaing <christophe.castaing@cdbdx.biz>
2020-12-08 12:14:26 +0100

    feat: 🎸 use SuhsilitWithFilter on app

--- src/SushiListWithFilter/useStyles.js
+++ src/SushiListWithFilter/useStyles.js
@@ -3,7 +3,7 @@ import { makeStyles } from "@material-ui/core/styles";
 export default makeStyles({
   search: {
     margin: "0 auto",
-    width: "400",
+    width: "400px",
     display: "flex",
   },
   form: {
